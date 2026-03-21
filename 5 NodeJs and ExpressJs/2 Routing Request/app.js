42f09fd3c4ff4ccbd7d25bdacb841958359f1a2b
Saturu-Gojo <prashasth068@gmail.com>
2026-03-21 09:25:18 +0530

    Added 2 Routing request

---
+++ 5 NodeJs and ExpressJs/2 Routing Request/app.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const fs = require('fs');
+
+console.log('I was here');
+
+const requestHandler = (req, res) => {
+    console.log('Handling request for:', req.url);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    // Use req.url to check where the user is going
+    if (req.url === "/") {
+        res.write(`
+           <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                
+                <title>Myntra</title>
+            </head>
+            <body>
+                <h1>Welcome to First Server</h1>
+                <form action="/buy-product" method = "POST">
+                    <input type="text" placeholder = "Enter the Product" name="productname">
+                    <input type="Number" placeholder = "Enter the Price" name="Price">
+                    <input type="submit">
+                </form>
+                
+            </body>
+            </html>
+        `);
+    } else if (req.url === "/products") {
+        res.write(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head><title>Products</title></head>
+            <body>
+                <h1>Product list will appear here.</h1>
+            </body>
+            </html>
+        `);
+    }else if(req.url === "/buy-product" ){
+        console.log("Form Data Recieved");
+        const arr = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            arr.push(chunk);
+        });
+        req.on('end',()=>{
+            const parseBody = Buffer.concat(arr).toString();
+            console.log(parseBody);
+        })
+        fs.writeFileSync('buy.txt',"Racket karid le");
+        res.statusCode=302;
+        res.setHeader('Location', '/products');
+        
+
+    }else {
+        res.statusCode=404;
+        // Handle 404 - Page Not Found
+        res.write('<h1>404: Page Not Found</h1>');
+    }
+
+    // Always call end() to tell the browser you're finished sending data
+    res.end('Done!');
+}
+
+const server = http.createServer(requestHandler);
+const port = 3001
+server.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
+});
+
